e2a1186ccb89e5d76b081d16a6bad122050e5b8f
dominik-zeglen <flesz3@o2.pl>
2018-06-03 17:31:03 +0200

    Improve tests

--- mongodb/containers_test.go
+++ mongodb/containers_test.go
@@ -5,182 +5,181 @@ import (
 
 	"github.com/bradleyjkemp/cupaloy"
 	"github.com/dominik-zeglen/ecoknow/core"
+	"github.com/globalsign/mgo/bson"
 )
 
 func init() {
 	resetDatabase()
 }
 
-func TestAddContainer(t *testing.T) {
-	container := core.Container{
-		Name:     "New Container",
-		ParentID: "000000000001",
-	}
-	result, err := dataSource.AddContainer(container)
-	id := result.ID
-	result.ID = ""
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-	dataSource.RemoveContainer(id)
-}
-
-func TestAddContainerWithoutName(t *testing.T) {
-	container := core.Container{
-		ParentID: "000000000001",
-	}
-	_, err := dataSource.AddContainer(container)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestBuildTree(t *testing.T) {
-	parent := core.Container{
-		ID:   "200000000000",
-		Name: "Parent",
-	}
-	result, err := dataSource.AddContainer(parent)
-	parentID := result.ID
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-
-	child := core.Container{
-		ID:       "200000000001",
-		Name:     "Child",
-		ParentID: parentID,
-	}
-	result, err = dataSource.AddContainer(child)
-	childID := result.ID
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err = ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotMulti("Child", data)
-
-	resultList, err := dataSource.GetContainerChildrenList(parentID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err = ToJSON(resultList)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotMulti("Parent's children", data)
-
-	dataSource.RemoveContainer(childID)
-	dataSource.RemoveContainer(parentID)
-}
-
-func TestGetContainer(t *testing.T) {
-	result, err := dataSource.GetContainer("000000000001")
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetNonExistingContainer(t *testing.T) {
-	_, err := dataSource.GetContainer("000000000099")
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestGetRootContainerList(t *testing.T) {
-	result, _ := dataSource.GetRootContainerList()
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetContainerChildren(t *testing.T) {
-	result, _ := dataSource.GetContainerChildrenList("000000000001")
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetContainerList(t *testing.T) {
-	result, _ := dataSource.GetContainerList()
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestUpdateContainer(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.UpdateContainer("000000000001", core.UpdateContainerArguments{
-		Name:     "Updated container name",
-		ParentID: "000000000002",
+func TestContainers(t *testing.T) {
+	t.Run("Test setters", func(t *testing.T) {
+		t.Run("Add container", func(t *testing.T) {
+			container := core.Container{
+				Name:     "New Container",
+				ParentID: containers[0].ID,
+			}
+			result, err := dataSource.AddContainer(container)
+			id := result.ID
+			result.ID = ""
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+			dataSource.RemoveContainer(id)
+		})
+		t.Run("Add container without name", func(t *testing.T) {
+			container := core.Container{
+				ParentID: containers[0].ID,
+			}
+			_, err := dataSource.AddContainer(container)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Update container", func(t *testing.T) {
+			defer resetDatabase()
+			parentID := bson.ObjectId(containers[1].ID)
+			err := dataSource.UpdateContainer(containers[0].ID, core.ContainerInput{
+				Name:     "Updated container name",
+				ParentID: &parentID,
+			})
+			if err != nil {
+				t.Error(err)
+			}
+			result, err := dataSource.GetContainer(containers[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Update container's name", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.UpdateContainer(containers[3].ID, core.ContainerInput{
+				Name: "Updated container name",
+			})
+			if err != nil {
+				t.Error(err)
+			}
+			result, err := dataSource.GetContainer(containers[3].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Remove container", func(t *testing.T) {
+			err := dataSource.RemoveContainer(containers[3].ID)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+		t.Run("Remove container that does not exist", func(t *testing.T) {
+			err := dataSource.RemoveContainer("000000000099")
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
 	})
-	if err != nil {
-		t.Error(err)
-	}
-	result, err := dataSource.GetContainer("000000000001")
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestUpdateContainerName(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.UpdateContainer("000000000004", core.UpdateContainerArguments{
-		Name: "Updated container name",
+	t.Run("Test getters", func(t *testing.T) {
+		t.Run("Get container", func(t *testing.T) {
+			result, err := dataSource.GetContainer(containers[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Get container that does not exist", func(t *testing.T) {
+			_, err := dataSource.GetContainer("000000000099")
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Get root container list", func(t *testing.T) {
+			result, _ := dataSource.GetRootContainerList()
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Get container's children", func(t *testing.T) {
+			result, _ := dataSource.GetContainerChildrenList(containers[0].ID)
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Get all containers", func(t *testing.T) {
+			result, _ := dataSource.GetContainerList()
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
 	})
-	if err != nil {
-		t.Error(err)
-	}
-	result, err := dataSource.GetContainer("000000000004")
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
+	t.Run("Test complex behaviour", func(t *testing.T) {
+		t.Run("Build container tree", func(t *testing.T) {
+			parent := core.Container{
+				ID:   "200000000000",
+				Name: "Parent",
+			}
+			result, err := dataSource.AddContainer(parent)
+			parentID := result.ID
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
 
-func TestRemoveContainer(t *testing.T) {
-	err := dataSource.RemoveContainer("000000000004")
-	if err != nil {
-		t.Error(err)
-	}
-}
+			child := core.Container{
+				ID:       "200000000001",
+				Name:     "Child",
+				ParentID: parentID,
+			}
+			result, err = dataSource.AddContainer(child)
+			childID := result.ID
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err = ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotMulti("Child", data)
 
-func TestRemoveNonExistingContainer(t *testing.T) {
-	err := dataSource.RemoveContainer("000000000099")
-	if err == nil {
-		t.Error(ErrNoError)
-	}
+			resultList, err := dataSource.GetContainerChildrenList(parentID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err = ToJSON(resultList)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotMulti("Parent's children", data)
+
+			dataSource.RemoveContainer(childID)
+			dataSource.RemoveContainer(parentID)
+		})
+	})
 }
--- mongodb/page_test.go
+++ mongodb/page_test.go
@@ -11,291 +11,282 @@ func init() {
 	resetDatabase()
 }
 
-func TestAddPage(t *testing.T) {
-	defer resetDatabase()
-	page := core.Page{
-		Name:     "New Page",
-		Slug:     "new-page",
-		ParentID: containers[0].ID,
-		Fields: []core.PageField{
-			core.PageField{
-				Type:  "unique",
-				Name:  "Field 1",
-				Value: "1",
-			},
-			core.PageField{
+func TestPages(t *testing.T) {
+	t.Run("Test setters", func(t *testing.T) {
+		t.Run("Add page", func(t *testing.T) {
+			defer resetDatabase()
+			page := core.Page{
+				Name:     "New Page",
+				Slug:     "new-page",
+				ParentID: containers[0].ID,
+				Fields: []core.PageField{
+					core.PageField{
+						Type:  "unique",
+						Name:  "Field 1",
+						Value: "1",
+					},
+					core.PageField{
+						Type:  "text",
+						Name:  "Field 2",
+						Value: "Some text",
+					},
+				},
+			}
+			result, err := dataSource.AddPage(page)
+			result.ID = ""
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Add page without parent", func(t *testing.T) {
+			defer resetDatabase()
+			page := core.Page{
+				Name: "New Page",
+				Slug: "new-page",
+				Fields: []core.PageField{
+					core.PageField{
+						Type:  "unique",
+						Name:  "Field 1",
+						Value: "1",
+					},
+					core.PageField{
+						Type:  "text",
+						Name:  "Field 2",
+						Value: "Some text",
+					},
+				},
+			}
+			_, err := dataSource.AddPage(page)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Add page with another's page slug", func(t *testing.T) {
+			defer resetDatabase()
+			page := core.Page{
+				Name: "New Page",
+				Slug: pages[0].Slug,
+				Fields: []core.PageField{
+					core.PageField{
+						Type:  "unique",
+						Name:  "Field 1",
+						Value: "1",
+					},
+					core.PageField{
+						Type:  "text",
+						Name:  "Field 2",
+						Value: "Some text",
+					},
+				},
+			}
+			_, err := dataSource.AddPage(page)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Add page with duplicated fields", func(t *testing.T) {
+			defer resetDatabase()
+			page := core.Page{
+				Name: "New Page",
+				Slug: pages[0].Slug,
+				Fields: []core.PageField{
+					core.PageField{
+						Type:  "unique",
+						Name:  "Field 1",
+						Value: "1",
+					},
+					core.PageField{
+						Type:  "text",
+						Name:  "Field 1",
+						Value: "Some text",
+					},
+				},
+			}
+			_, err := dataSource.AddPage(page)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Add page from a template", func(t *testing.T) {
+			defer resetDatabase()
+			result, err := dataSource.AddPageFromTemplate(
+				core.PageInput{
+					Name:     "New page",
+					ParentID: containers[0].ID,
+				},
+				templates[0].ID,
+			)
+			result.ID = ""
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Add field to page", func(t *testing.T) {
+			defer resetDatabase()
+			field := core.PageField{
 				Type:  "text",
-				Name:  "Field 2",
-				Value: "Some text",
-			},
-		},
-	}
-	result, err := dataSource.AddPage(page)
-	result.ID = ""
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestAddPageWithoutParentID(t *testing.T) {
-	defer resetDatabase()
-	page := core.Page{
-		Name: "New Page",
-		Slug: "new-page",
-		Fields: []core.PageField{
-			core.PageField{
-				Type:  "unique",
-				Name:  "Field 1",
-				Value: "1",
-			},
-			core.PageField{
-				Type:  "text",
-				Name:  "Field 2",
-				Value: "Some text",
-			},
-		},
-	}
-	_, err := dataSource.AddPage(page)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestAddPageWithExistingSlug(t *testing.T) {
-	defer resetDatabase()
-	page := core.Page{
-		Name: "New Page",
-		Slug: pages[0].Slug,
-		Fields: []core.PageField{
-			core.PageField{
-				Type:  "unique",
-				Name:  "Field 1",
-				Value: "1",
-			},
-			core.PageField{
-				Type:  "text",
-				Name:  "Field 2",
-				Value: "Some text",
-			},
-		},
-	}
-	_, err := dataSource.AddPage(page)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestAddPageWithDuplicatedField(t *testing.T) {
-	defer resetDatabase()
-	page := core.Page{
-		Name: "New Page",
-		Slug: pages[0].Slug,
-		Fields: []core.PageField{
-			core.PageField{
-				Type:  "unique",
-				Name:  "Field 1",
-				Value: "1",
-			},
-			core.PageField{
-				Type:  "text",
-				Name:  "Field 1",
-				Value: "Some text",
-			},
-		},
-	}
-	_, err := dataSource.AddPage(page)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestAddFieldToPage(t *testing.T) {
-	defer resetDatabase()
-	field := core.PageField{
-		Type:  "text",
-		Name:  "New Field",
-		Value: "New Value",
-	}
-	err := dataSource.AddPageField(pages[0].ID, field)
-	if err != nil {
-		t.Error(err)
-	}
-	page, err := dataSource.GetPage(pages[0].ID)
-	data, err := ToJSON(page)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestAddFieldToPageWithNonExistingType(t *testing.T) {
-	defer resetDatabase()
-	field := core.PageField{
-		Type:  "idontexist",
-		Name:  "New Field",
-		Value: "New Value",
-	}
-	err := dataSource.AddPageField(pages[0].ID, field)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestAddExistingFieldToPage(t *testing.T) {
-	defer resetDatabase()
-	field := core.PageField{Type: "text", Name: "Field 1"}
-	err := dataSource.AddPageField(pages[0].ID, field)
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestAddPageFromTemplate(t *testing.T) {
-	defer resetDatabase()
-	result, err := dataSource.AddPageFromTemplate(
-		"New page",
-		containers[0].ID,
-		templates[0],
-	)
-	result.ID = ""
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetPage(t *testing.T) {
-	result, err := dataSource.GetPage(pages[0].ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetPageBySlug(t *testing.T) {
-	result, err := dataSource.GetPageBySlug(pages[0].Slug)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestGetPagesFromContainer(t *testing.T) {
-	result, err := dataSource.GetPagesFromContainer(containers[0].ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data, err := ToJSON(result)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestUpdatePage(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.UpdatePage(pages[0].ID, core.UpdatePageArguments{
-		Name: "Updated page name",
+				Name:  "New Field",
+				Value: "New Value",
+			}
+			err := dataSource.AddPageField(pages[0].ID, field)
+			if err != nil {
+				t.Error(err)
+			}
+			page, err := dataSource.GetPage(pages[0].ID)
+			data, err := ToJSON(page)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Add field of unknown type to page", func(t *testing.T) {
+			defer resetDatabase()
+			field := core.PageField{
+				Type:  "idontexist",
+				Name:  "New Field",
+				Value: "New Value",
+			}
+			err := dataSource.AddPageField(pages[0].ID, field)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Add field to page with name of another field", func(t *testing.T) {
+			defer resetDatabase()
+			field := core.PageField{Type: "text", Name: "Field 1"}
+			err := dataSource.AddPageField(pages[0].ID, field)
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Update page", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.UpdatePage(pages[0].ID, core.UpdatePageArguments{
+				Name: "Updated page name",
+			})
+			if err != nil {
+				t.Error(err)
+			}
+			page, err := dataSource.GetPage(pages[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(page)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Update page with another's page slug", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.UpdatePage(pages[0].ID, core.UpdatePageArguments{
+				Slug: pages[1].Slug,
+			})
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Update page's field", func(t *testing.T) {
+			err := dataSource.UpdatePageField(pages[0].ID, "Field 1", "99")
+			if err != nil {
+				t.Error(err)
+			}
+			page, err := dataSource.GetPage(pages[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(page)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Update page's field with unknown type", func(t *testing.T) {
+			err := dataSource.UpdatePageField(pages[0].ID, "Field 3", "99")
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
+		t.Run("Remove page", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.RemovePage(pages[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			pages, err := dataSource.GetPagesFromContainer(containers[0].ID)
+			if err != nil {
+				t.Error(err)
+			}
+			data, err := ToJSON(pages)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Remove page's field", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.RemovePageField(pages[0].ID, "Field 1")
+			if err != nil {
+				t.Error(err)
+			}
+			page, err := dataSource.GetPage(pages[0].ID)
+			data, err := ToJSON(page)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Remove page's field that does not exist", func(t *testing.T) {
+			defer resetDatabase()
+			err := dataSource.RemovePageField(pages[0].ID, "Field 3")
+			if err == nil {
+				t.Error(ErrNoError)
+			}
+		})
 	})
-	if err != nil {
-		t.Error(err)
-	}
-	page, err := dataSource.GetPage(pages[0].ID)
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(page)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestUpdatePageWithExistingSlug(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.UpdatePage(pages[0].ID, core.UpdatePageArguments{
-		Slug: pages[1].Slug,
+	t.Run("Test getters", func(t *testing.T) {
+		t.Run("Get page", func(t *testing.T) {
+			result, err := dataSource.GetPage(pages[0].ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Get page by slug", func(t *testing.T) {
+			result, err := dataSource.GetPageBySlug(pages[0].Slug)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
+		t.Run("Get pages from container", func(t *testing.T) {
+			result, err := dataSource.GetPagesFromContainer(containers[0].ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := ToJSON(result)
+			if err != nil {
+				t.Error(err)
+			}
+			cupaloy.SnapshotT(t, data)
+		})
 	})
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestUpdatePageField(t *testing.T) {
-	err := dataSource.UpdatePageField(pages[0].ID, "Field 1", "99")
-	if err != nil {
-		t.Error(err)
-	}
-	page, err := dataSource.GetPage(pages[0].ID)
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(page)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestUpdateNonExistingPageField(t *testing.T) {
-	err := dataSource.UpdatePageField(pages[0].ID, "Field 3", "99")
-	if err == nil {
-		t.Error(ErrNoError)
-	}
-}
-
-func TestRemovePage(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.RemovePage(pages[0].ID)
-	if err != nil {
-		t.Error(err)
-	}
-	pages, err := dataSource.GetPagesFromContainer(containers[0].ID)
-	if err != nil {
-		t.Error(err)
-	}
-	data, err := ToJSON(pages)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestRemoveFieldFromPage(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.RemovePageField(pages[0].ID, "Field 1")
-	if err != nil {
-		t.Error(err)
-	}
-	page, err := dataSource.GetPage(pages[0].ID)
-	data, err := ToJSON(page)
-	if err != nil {
-		t.Error(err)
-	}
-	cupaloy.SnapshotT(t, data)
-}
-
-func TestRemoveNonExistentFieldFromPage(t *testing.T) {
-	defer resetDatabase()
-	err := dataSource.RemovePageField(pages[0].ID, "Field 3")
-	if err == nil {
-		t.Error(ErrNoError)
-	}
 }
