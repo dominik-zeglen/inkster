e8f4ddfb690fe682d1877265fb92abbc54cc4e04
dominik-zeglen <flesz3@o2.pl>
2018-09-29 16:37:38 +0200

    Add messages in UI

--- panel/src/pages/views/PageCreate.tsx
+++ panel/src/pages/views/PageCreate.tsx
@@ -4,6 +4,7 @@ import { ApolloError } from "apollo-client";
 
 import { urls, TransactionState } from "../../";
 import Navigator from "../../components/Navigator";
+import Notificator, { NotificationType } from "../../components/Notificator";
 import PageCreatePage, { FormData } from "../components/PageCreatePage";
 import mPageCreate, {
   result as PageCreateResult,
@@ -35,72 +36,89 @@ export class PageCreate extends React.Component<Props, State> {
   render() {
     const { directory } = this.props;
     return (
-      <Navigator>
-        {navigate => {
-          const handleBack = () => navigate(urls.directoryDetails(directory));
-          const handleSubmitSuccess = (data: {
-            createPage: PageCreateResult;
-          }) => {
-            if (
-              data.createPage &&
-              data.createPage.userErrors &&
-              data.createPage.userErrors.length > 0
-            ) {
-              return;
-            }
-            navigate(urls.pageDetails(data.createPage.page.id));
-          };
-          return (
-            <WithUpload>
-              {uploadFile => {
-                const handleUpload = (onChange: any) => (
-                  event: React.ChangeEvent<any>
-                ) => {
-                  uploadFile.uploadFile({
-                    file: event.target.files[0],
-                    onSuccess: filename =>
-                      onChange({
-                        target: {
-                          name: "value",
-                          value: filename
-                        }
-                      } as any),
-                    onError: () => console.log("not ok")
-                  });
-                };
-                return (
-                  <Mutation
-                    mutation={mPageCreate}
-                    onCompleted={handleSubmitSuccess}
-                  >
-                    {(createPage, { data, loading }) => {
-                      const handleSubmit = (formData: FormData) =>
-                        createPage({
-                          variables: {
-                            name: formData.name,
-                            parentId: directory,
-                            fields: formData.addFields
-                          } as PageCreateVariables
-                        });
-                      return (
-                        <PageCreatePage
-                          disabled={loading}
-                          loading={loading}
-                          title={i18n.t("Create new page")}
-                          transaction={this.state.transaction}
-                          onBack={handleBack}
-                          onUpload={handleUpload}
-                          onSubmit={handleSubmit}
-                        />
-                      );
-                    }}
-                  </Mutation>
-                );
-              }}
-            </WithUpload>
-          );
-        }}
-      </Navigator>
+      <Notificator>
+        {notify => (
+          <Navigator>
+            {navigate => {
+              const handleBack = () =>
+                navigate(urls.directoryDetails(directory));
+              const handleError = () =>
+                notify({
+                  text: i18n.t("Something has gone wrong", {
+                    context: "notification"
+                  }),
+                  type: NotificationType.ERROR
+                });
+              const handleSubmitSuccess = (data: {
+                createPage: PageCreateResult;
+              }) => {
+                if (
+                  data.createPage &&
+                  data.createPage.userErrors &&
+                  data.createPage.userErrors.length > 0
+                ) {
+                  handleError();
+                }
+                notify({
+                  text: i18n.t("Page created", {
+                    context: "notification"
+                  })
+                });
+                navigate(urls.pageDetails(data.createPage.page.id));
+              };
+              return (
+                <WithUpload>
+                  {uploadFile => {
+                    const handleUpload = (onChange: any) => (
+                      event: React.ChangeEvent<any>
+                    ) => {
+                      uploadFile.uploadFile({
+                        file: event.target.files[0],
+                        onSuccess: filename =>
+                          onChange({
+                            target: {
+                              name: "value",
+                              value: filename
+                            }
+                          } as any),
+                        onError: () => console.log("not ok")
+                      });
+                    };
+                    return (
+                      <Mutation
+                        mutation={mPageCreate}
+                        onCompleted={handleSubmitSuccess}
+                      >
+                        {(createPage, { data, loading }) => {
+                          const handleSubmit = (formData: FormData) =>
+                            createPage({
+                              variables: {
+                                name: formData.name,
+                                parentId: directory,
+                                fields: formData.addFields
+                              } as PageCreateVariables
+                            });
+                          return (
+                            <PageCreatePage
+                              disabled={loading}
+                              loading={loading}
+                              title={i18n.t("Create new page")}
+                              transaction={this.state.transaction}
+                              onBack={handleBack}
+                              onUpload={handleUpload}
+                              onSubmit={handleSubmit}
+                            />
+                          );
+                        }}
+                      </Mutation>
+                    );
+                  }}
+                </WithUpload>
+              );
+            }}
+          </Navigator>
+        )}
+      </Notificator>
     );
   }
 }
--- panel/src/users/views/UserDetails.tsx
+++ panel/src/users/views/UserDetails.tsx
@@ -1,49 +1,97 @@
 import * as React from "react";
 import { Mutation, Query } from "react-apollo";
 
-import mUpdateUser from "../queries/mUpdateUser";
+import mUpdateUser, {
+  Result as UpdateUserResult
+} from "../queries/mUpdateUser";
 import mRemoveUser from "../queries/mRemoveUser";
 import qUser from "../queries/qUser";
 import UserDetailsPage from "../components/UserDetailsPage";
 import Navigator from "../../components/Navigator";
+import Notificator, { NotificationType } from "../../components/Notificator";
+import i18n from "../../i18n";
 
 interface Props {
   id: string;
 }
 
 export const UserDetails: React.StatelessComponent<Props> = ({ id }) => (
-  <Navigator>
-    {navigate => {
-      const handleRemoveUser = () => navigate("/users/");
-      return (
-        <Query query={qUser} variables={{ id }}>
-          {userData => (
-            <Mutation mutation={mRemoveUser} onCompleted={handleRemoveUser}>
-              {removeUser => (
-                <Mutation mutation={mUpdateUser}>
-                  {updateUser => (
-                    <UserDetailsPage
-                      disabled={userData.loading}
-                      loading={userData.loading}
-                      onBack={() => navigate("/users/")}
-                      onDelete={() => removeUser({ variables: { id } })}
-                      title={
-                        userData.data && userData.data.user
-                          ? userData.data.user.email
-                          : undefined
-                      }
-                      transaction={userData.loading ? "loading" : "default"}
-                      user={userData.data ? userData.data.user : undefined}
-                      onSubmit={variables => updateUser({variables: {id, input: variables}})}
-                    />
+  <Notificator>
+    {notify => (
+      <Navigator>
+        {navigate => {
+          const handleError = () =>
+            notify({
+              text: i18n.t("Something went wrong", {
+                context: "notification"
+              }),
+              type: NotificationType.ERROR
+            });
+          const handleUpdateUser = (data: UpdateUserResult) => {
+            if (
+              data &&
+              data.updateUser &&
+              data.updateUser.errors &&
+              data.updateUser.errors.length > 0
+            ) {
+              handleError();
+            } else {
+              notify({
+                text: i18n.t("Updated user", {
+                  context: "notification"
+                })
+              });
+            }
+          };
+          const handleRemoveUser = () => {
+            notify({
+              text: i18n.t("Deleted user", {
+                context: "notification"
+              })
+            });
+            navigate("/users/");
+          };
+          return (
+            <Query query={qUser} variables={{ id }}>
+              {userData => (
+                <Mutation
+                  mutation={mRemoveUser}
+                  onCompleted={handleRemoveUser}
+                  onError={handleError}
+                >
+                  {removeUser => (
+                    <Mutation
+                      mutation={mUpdateUser}
+                      onCompleted={handleUpdateUser}
+                      onError={handleError}
+                    >
+                      {updateUser => (
+                        <UserDetailsPage
+                          disabled={userData.loading}
+                          loading={userData.loading}
+                          onBack={() => navigate("/users/")}
+                          onDelete={() => removeUser({ variables: { id } })}
+                          title={
+                            userData.data && userData.data.user
+                              ? userData.data.user.email
+                              : undefined
+                          }
+                          transaction={userData.loading ? "loading" : "default"}
+                          user={userData.data ? userData.data.user : undefined}
+                          onSubmit={variables =>
+                            updateUser({ variables: { id, input: variables } })
+                          }
+                        />
+                      )}
+                    </Mutation>
                   )}
                 </Mutation>
               )}
-            </Mutation>
-          )}
-        </Query>
-      );
-    }}
-  </Navigator>
+            </Query>
+          );
+        }}
+      </Navigator>
+    )}
+  </Notificator>
 );
 export default UserDetails;
