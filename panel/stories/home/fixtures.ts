1dbc0ff94969e7203a984dfd0556851d9d051bfe
dominik-zeglen <flesz3@o2.pl>
2018-12-16 02:01:34 +0100

    Add typenames

--- panel/stories/home/fixtures.ts
+++ panel/stories/home/fixtures.ts
@@ -1,8 +1,12 @@
-export const viewer = {
+import { Viewer_viewer } from "../../src/home/queries/types/Viewer";
+
+export const viewer: Viewer_viewer = {
+  __typename: "User",
   id: "dXNlcjox",
   email: "admin@example.com",
   pages: [
     {
+      __typename: "Page",
       id: "cGFnZTox",
       name: "How hairless apes evolved to build civilisation",
       slug: "how-apes-build-civilisation",
