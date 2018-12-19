af4b152df5b7d758b81cd1354b752d37bdcdcedf
dominik-zeglen <flesz3@o2.pl>
2018-12-19 01:34:04 +0100

    Add page sorting

--- api/pages_queries.go
+++ api/pages_queries.go
@@ -70,10 +70,10 @@ func (res *Resolver) Pages(
 	query := res.
 		dataSource.
 		DB().
-		Model(&pages).
-		OrderExpr("id ASC")
-
+		Model(&pages)
+	query = sortPages(query, args.Sort)
 	err := query.Select()
+
 	if err != nil {
 		return nil, err
 	}
@@ -87,4 +87,29 @@ func (res *Resolver) Pages(
 	}
 
 	return &resolvers, nil
+}
+
+func sortPages(query *orm.Query, sort *PageSort) *orm.Query {
+	if sort != nil {
+		switch sort.Field {
+		case "AUTHOR":
+			query = query.Relation("Author")
+			return query.OrderExpr("Author.email " + sort.Order)
+
+		case "IS_PUBLISHED":
+			return query.OrderExpr("is_published " + sort.Order)
+
+		case "NAME":
+			return query.OrderExpr("name " + sort.Order)
+
+		case "SLUG":
+			return query.OrderExpr("slug " + sort.Order)
+
+		case "UPDATED_AT":
+			return query.OrderExpr("updated_at " + sort.Order)
+		}
+
+	}
+
+	return query.OrderExpr("created_at ASC")
 }
