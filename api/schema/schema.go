7df4585297f796738084b05890bbf6464a110618
Dominik <anders@localhost.localdomain>
2019-08-02 18:45:25 +0200

    Fix schema import

--- api/schema/schema.go
+++ api/schema/schema.go
@@ -14,7 +14,7 @@ func String() string {
 	sort.Strings(assets)
 
 	for _, name := range assets {
-		b := MustAsset(name)
+		b, _ := Asset(name)
 		buf.Write(b)
 
 		if len(b) > 0 && b[len(b)-1] != '\n' {
