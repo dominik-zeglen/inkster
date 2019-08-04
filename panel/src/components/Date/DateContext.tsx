4318bcf24d7b374f351289921aa9e473670376f3
Dominik Żegleń <flesz3@o2.pl>
2019-08-05 01:31:21 +0200

    Remove panel (#101)
    * Remove panel
    
    * Update travis config

--- panel/src/components/Date/DateContext.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-const { Provider, Consumer } = React.createContext<number>(0);
-
-export { Consumer, Provider };
--- panel/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Modal<T extends string> {
-  modal: T;
-}
-export interface Pagination {
-  after: string;
-  before: string;
-}
