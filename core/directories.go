dfee73f5e8d5f9070e336df8437e95d4ed21cf93
Dominik Żegleń <flesz3@o2.pl>
2019-10-14 00:57:11 +0200

    Move relation validation to model validation

--- core/directories.go
+++ core/directories.go
@@ -17,8 +17,11 @@ func (directory Directory) String() string {
 	return fmt.Sprintf("Directory<%s>", directory.Name)
 }
 
-func (directory Directory) Validate() []ValidationError {
+func (directory Directory) Validate(
+	dataSource AbstractDataContext,
+) ([]ValidationError, error) {
 	validationErrors := ValidateModel(directory)
+
 	if directory.ParentID != nil {
 		if directory.ID == *directory.ParentID {
 			message := "its ID"
@@ -29,5 +32,20 @@ func (directory Directory) Validate() []ValidationError {
 			})
 		}
 	}
-	return validationErrors
+
+	if directory.ParentID != nil {
+		exists, err := checkIfDirectoryExist(*directory.ParentID, dataSource)
+		if err != nil {
+			return validationErrors, err
+		}
+
+		if !exists {
+			validationErrors = append(validationErrors, ValidationError{
+				Code:  ErrDoesNotExist,
+				Field: "ParentID",
+			})
+		}
+	}
+
+	return validationErrors, nil
 }
--- core/directories_actions.go
+++ core/directories_actions.go
@@ -4,30 +4,16 @@ func CreateDirectory(
 	directory Directory,
 	dataSource AbstractDataContext,
 ) (*Directory, []ValidationError, error) {
-	validationErrors := directory.Validate()
+	validationErrors, err := directory.Validate(dataSource)
 
-	if directory.ParentID != nil {
-		exists, err := checkIfDirectoryExist(*directory.ParentID, dataSource)
-		if err != nil {
-			return nil, validationErrors, err
-		}
-
-		if !exists {
-			validationErrors = append(validationErrors, ValidationError{
-				Code:  ErrDoesNotExist,
-				Field: "ParentID",
-			})
-		}
-	}
-
-	if len(validationErrors) > 0 {
-		return nil, validationErrors, nil
+	if err != nil || len(validationErrors) > 0 {
+		return nil, validationErrors, err
 	}
 
 	directory.CreatedAt = dataSource.GetCurrentTime()
 	directory.UpdatedAt = dataSource.GetCurrentTime()
 
-	_, err := dataSource.
+	_, err = dataSource.
 		DB().
 		Model(&directory).
 		Insert()
@@ -39,36 +25,15 @@ func UpdateDirectory(
 	directory Directory,
 	dataSource AbstractDataContext,
 ) (*Directory, []ValidationError, error) {
-	validationErrors := directory.Validate()
+	validationErrors, err := directory.Validate(dataSource)
 
-	if directory.ParentID != nil {
-		parent := Directory{}
-		parent.ID = *directory.ParentID
-		exists, err := dataSource.
-			DB().
-			Model(&parent).
-			WherePK().
-			Exists()
-
-		if err != nil {
-			return nil, validationErrors, err
-		}
-
-		if !exists {
-			validationErrors = append(validationErrors, ValidationError{
-				Code:  ErrDoesNotExist,
-				Field: "ParentID",
-			})
-		}
-	}
-
-	if len(validationErrors) > 0 {
-		return nil, validationErrors, nil
+	if err != nil || len(validationErrors) > 0 {
+		return nil, validationErrors, err
 	}
 
 	directory.UpdatedAt = dataSource.GetCurrentTime()
 
-	_, err := dataSource.
+	_, err = dataSource.
 		DB().
 		Model(&directory).
 		WherePK().
