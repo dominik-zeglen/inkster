febb77517c045d511012f28f28cdae5a3da4a52d
Dominik Żegleń <flesz3@o2.pl>
2019-11-28 02:21:35 +0100

    Migrate to jsonb

--- migrations/6_use_page_fields_jsonb.go
+++ migrations/6_use_page_fields_jsonb.go
@@ -1,71 +1,82 @@
 package main
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/go-pg/migrations"
 	"github.com/go-pg/pg/orm"
 )
 
-type Page struct {
-	ID          int         `sql:",pk,autoincrement" json:"id"`
-	CreatedAt   time.Time   `sql:",notnull" json:"createdAt" bson:"createdAt"`
-	UpdatedAt   time.Time   `sql:",notnull" json:"updatedAt" bson:"updatedAt"`
-	AuthorID    int         `sql:",notnull" json:"authorId" validate:"required"`
-	Name        string      `sql:",notnull" json:"name" validate:"required,min=3"`
-	Slug        string      `sql:",notnull" json:"slug" validate:"omitempty,slug,min=3"`
-	ParentID    int         `sql:",notnull" json:"parentId" validate:"required"`
-	IsPublished bool        `sql:",notnull" json:"isPublished"`
-	Fields      []PageField `json:"fields" sql:"type:jsonb" validate:"dive"`
+type pageType struct {
+	tableName   struct{}  `sql:"pages"`
+	ID          int       `sql:",pk,autoincrement" json:"id"`
+	CreatedAt   time.Time `sql:",notnull" json:"createdAt" bson:"createdAt"`
+	UpdatedAt   time.Time `sql:",notnull" json:"updatedAt" bson:"updatedAt"`
+	AuthorID    int       `sql:",notnull" json:"authorId" validate:"required"`
+	Name        string    `sql:",notnull" json:"name" validate:"required,min=3"`
+	Slug        string    `sql:",notnull" json:"slug" validate:"omitempty,slug,min=3"`
+	ParentID    int       `sql:",notnull" json:"parentId" validate:"required"`
+	IsPublished bool      `sql:",notnull" json:"isPublished"`
 }
-type PageField struct {
+type pageFieldType struct {
+	tableName struct{}  `sql:"page_fields"`
 	ID        int       `sql:",pk,autoincrement" json:"id"`
 	CreatedAt time.Time `sql:",notnull" json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time `sql:",notnull" json:"updatedAt" bson:"updatedAt"`
 	PageID    int       `sql:",notnull,on_delete:CASCADE" json:"-"`
 	Type      string    `json:"type" validate:"required,oneof=text longText image file"`
-	Slug      string    `json:"name" validate:"required,slug"`
+	Name      string    `json:"name" validate:"required,min=3"`
 	Value     string    `json:"value"`
+}
+type jsonPageFieldType struct {
+	Type  string `json:"type" validate:"required,oneof=text longText image file"`
+	Slug  string `json:"name" validate:"required,slug"`
+	Value string `json:"value"`
 }
 
 func init() {
 	migrations.MustRegisterTx(
 		func(db migrations.DB) error {
-			_, err := orm.
-				NewQuery(db, nil).
-				Exec("ALTER TABLE pages ADD COLUMN page_fields jsonb not null default '[]'::jsonb")
+			var pageFields []pageFieldType
+
+			err := orm.
+				NewQuery(db, &pageFields).
+				Select()
 
 			if err != nil {
 				return err
 			}
 
-			var pageFields []PageField
-
-			_, err = orm.
-				NewQuery(db, pageFields).
-				Exec("SELECT * FROM page_fields")
-
-			if err != nil {
-				return err
-			}
-
-			fieldMap := make(map[int]([]PageField))
+			fieldMap := make(map[int]([]jsonPageFieldType))
 
 			for _, field := range pageFields {
-				fieldMap[field.PageID] = append(fieldMap[field.PageID], field)
+				fieldMap[field.PageID] = append(
+					fieldMap[field.PageID],
+					jsonPageFieldType{
+						Type:  field.Type,
+						Slug:  field.Name,
+						Value: field.Value,
+					})
+			}
+
+			_, err = orm.
+				NewQuery(db, nil).
+				Exec("ALTER TABLE pages ADD COLUMN fields jsonb not null default '[]'::jsonb")
+
+			if err != nil {
+				return err
 			}
 
 			for id, fields := range fieldMap {
-				page := Page{
-					ID:     id,
-					Fields: fields,
-				}
-
+				jsonFields, _ := json.Marshal(fields)
 				_, err := orm.
 					NewQuery(db, nil).
-					Model(&page).
-					WherePK().
-					UpdateNotNull()
+					Exec(
+						"UPDATE pages SET fields = ? WHERE id = ?",
+						string(jsonFields),
+						id,
+					)
 
 				if err != nil {
 					return err
